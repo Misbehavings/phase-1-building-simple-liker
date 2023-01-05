7096fd10335c1a0000f4d59f012dd48bc4ca8fd0
Yun Chen <Yunchenfly@gmail.com>
2023-01-05 11:43:14 -0500

    commit tree c5b5e46e05c380fbab3a5f5ca301d065caa8e523

--- main.js
+++ main.js
@@ -3,10 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal');
+modal.className = 'hidden';
+const modalMsg = document.getElementById('modal-message')
 
+const hearts = document.querySelectorAll('.like-glyph')
 
+for (let heart of hearts) {
+  heart.addEventListener('click', toggleHeart)
+}
 
-
+function toggleHeart(e) {
+  console.log(`${e.target.innerHTML} clicked`);
+  let heart = e.target
+  mimicServerCall()
+    .then(function(serverMessage) {
+      alert(serverMessage);
+      if (heart.innerText == EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.classList.add('.activated-heart');
+      }
+      else {
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove('.activated-heart');
+      }
+    })
+    .catch((error) => {
+      modal.classList.remove('hidden')
+      modalMsg.innerHTML += error
+      setTimeout(() => modal.className = 'hidden', 5000);
+    })
+};
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
